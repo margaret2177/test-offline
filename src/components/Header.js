04f260aa775aaa0a3a901cea6d96d9078d390ee1
karl <jstadcyccnt@gmail.com>
2023-02-20 22:10:34 +0800

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import styles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.left}>logo</div>
+      <div className={styles.left}>MeTube</div>
       <div className={styles.middle}>search</div>
       <div className={styles.right}>icons</div>
     </div>
